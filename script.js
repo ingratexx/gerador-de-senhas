d50c33c45f6cf3a7f2963c6cd6d8fbf66e782bf4
Ingrate <117471408+ingratexx@users.noreply.github.com>
2023-12-14 12:21:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function getPassword(){
+       
+    var letras = "qwertyuiopasdfghjklçzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    var numeros = "1234567890";
+    var caracteres = "!@#$%¨&*()_+`{^}>:?<=-[ç~].;/,";
+    var easyPassword = 8;
+    var intermediaryPassword = 10;
+    var hardPassword = 16;
+
+    document.querySelectorAll('input type="radio"'). forEach(function(radio) {
+        radio.addEventListener('click', function() )
+    })
+
+}
